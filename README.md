c65a94022bcc9b03fa3d08c0d719a4f2d204d523
z2g5497 <z2g5497@H1809481.intra.ctc-g.co.jp>
2018-12-21 20:35:48 +0900

    iframe埋め込み

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 ## TODO
 Evernote README.md化
+
 start=10で2ページ目が取れるらしい（何件取れるかはブラウザ設定）
+
+in a frame because it set 'X-Frame-Options' to 'sameorigin'.でAWSサイトが見れない！
+
+SearchPreviewはどうしているんだろう？
--- js/contents.js
+++ js/contents.js
@@ -10,25 +10,69 @@
 // TODO 名前Quarterにした方がいいかも
 
 $(function() {
+  // 名前空間
+  qt = {}
+
+  // TODO 名前再検討、名前空間も
+  qt.waiting = [] // 表示待ち
+  qt.paneling = {} // 表示中
+  qt.paneled = [] // 表示済み
+  qt.trush = {} // 削除済み
+
+  // console.log(iconURL)
+
+
+  // var self = this
+
+  // ボタン設置
   var iconURL = chrome.extension.getURL("images/icon.png")
   console.log(iconURL)
-// $('input[type="text"][name="q"]').after('<button class="quoter-button"><img
-// src="' + iconURL + '"></button>');
   $('div[class="voice_search_button"]').before('<div class="qcss-button qjs-button"><img src="' + iconURL + '" title="Search by Quoter"></div>')
-  $('div.qjs-button').click(function() {
+  $('div.qjs-button').click(() => {
     console.log("cliced!")
-    // TODO 親がHTTPSならHTTPSにする
+    var url = location.protocol + '//www.google.co.jp/search?q=' + $('input[name="q"]').val().replace(' ', '+')
+    console.log(url)
     $.ajax({
-      url:'https://www.google.co.jp/search?q=chrome',
-      type:'GET',
-      data:{}
+      url: url,
+      type:'GET'
     })
     .done( (data) => {
-//      console.log(data);
-      // TODO hrefを抜き出す処理
       var html = $($.parseHTML(data))
-//      html.filter('div.srg table.ts div.r a').forEach(function(element, index, ))
       console.log(html)
+      var titles = $('div.srg div.rc div.r a h3', $(data))
+      console.log(titles)
+      // var self = this
+      titles.each((i, elm) => {
+        // console.log(i + ':' + $(e).parent().attr('href'))
+        // console.log(i + ':' + $(e).html())
+        qt.waiting.push({
+          url: $(elm).parent().attr('href'),
+          title: $(elm).html()
+        })
+      })
+      console.log(qt.waiting)
+
+      // 描画
+      // TODO 元のhtml全部消していいのか（input残した方がいい？）
+      $.get(chrome.runtime.getURL('html/panels.html'), (data) => {
+        console.log(data)
+        $('html').html(data)
+        $('head').append('<link rel="stylesheet" href="' + chrome.extension.getURL("css/panels.css") + '" />')
+        $('div.qjs-panel').each((i, panel) => {
+          //$(panel).html('<iframe src="' + qt.waiting[i].url + '"></iframe>')
+          $.ajax({
+            url: qt.waiting[i].url,
+            type:'GET'
+          })
+          .done( (data) => {
+            console.log(data);
+            $(panel).html(data);
+          })
+          .fail( (data) => {
+            console.log(data);
+          })
+        })
+      })
     })
     .fail( (data) => {
       console.log(data);
