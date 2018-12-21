85a7fbd92555d33a6f19050119530ff54ea1d4ae
z2g5497 <z2g5497@H1809481.intra.ctc-g.co.jp>
2018-12-21 16:45:54 +0900

    ajax追加

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # quoter
+
+## TODO
+Evernote README.md化
+start=10で2ページ目が取れるらしい（何件取れるかはブラウザ設定）
--- js/contents.js
+++ js/contents.js
@@ -12,9 +12,28 @@
 $(function() {
   var iconURL = chrome.extension.getURL("images/icon.png")
   console.log(iconURL)
-//  $('input[type="text"][name="q"]').after('<button class="quoter-button"><img src="' + iconURL + '"></button>');
-  $('div[class="voice_search_button"]').before('<div class="qcss-button qjs-button"><img src="' + iconURL + '" title="Search by Ouoter"></div>')
+// $('input[type="text"][name="q"]').after('<button class="quoter-button"><img
+// src="' + iconURL + '"></button>');
+  $('div[class="voice_search_button"]').before('<div class="qcss-button qjs-button"><img src="' + iconURL + '" title="Search by Quoter"></div>')
   $('div.qjs-button').click(function() {
     console.log("cliced!")
+    // TODO 親がHTTPSならHTTPSにする
+    $.ajax({
+      url:'https://www.google.co.jp/search?q=chrome',
+      type:'GET',
+      data:{}
+    })
+    .done( (data) => {
+//      console.log(data);
+      // TODO hrefを抜き出す処理
+      var html = $($.parseHTML(data))
+//      html.filter('div.srg table.ts div.r a').forEach(function(element, index, ))
+      console.log(html)
+    })
+    .fail( (data) => {
+      console.log(data);
+    })
+    .always( (data) => {
+    })
   })
 })
